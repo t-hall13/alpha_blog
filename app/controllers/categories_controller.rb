d4c8d0a65d1c45128fe6bbc4c03df576db00bcb3
Tim Hall <timothypatrickhall@gmail.com>
2016-06-20 12:55:39 +0000

    Update pagination in Categories controller to 4 per page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 3)
+    @categories = Category.paginate(page: params[:page], per_page: 4)
   end
   
   def new
