87fbdea67457329cddb1ba86b28647290d5a874f
Tim Hall <timothypatrickhall@gmail.com>
2016-06-20 21:00:57 +0000

    Update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,8 @@ class CategoriesController < ApplicationController
     end
   end
   def show
-    
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 4)
   end
   
   private
